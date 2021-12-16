dc39c8f59b410f5575284731d771dd14e89b5128
eduardo <42901658+lelg322@users.noreply.github.com>
2021-12-16 00:24:35 -0500

    Update juego en terminal.py

--- juego en terminal.py
+++ juego en terminal.py
@@ -1,10 +1,13 @@
-
+import copy
 ambiente= [1,0,1,0,0,0,1,1,0,1,0]
 
 class arbol():
     def __init__(self):
         self.raiz = 0
         self.hijos=[]
+
+    def __str__(self):
+        return [self.raiz, self.hijos]
 
     def raiz(self):
         return self.raiz
@@ -24,19 +27,39 @@ def ponerLinea(N,ambiente):
     return ambientecopia
 
 def llenarArbol(ambiente):
-    arbol = arbol()
-    arbol.raiz=ambiente
+    arvol = arbol()
+    arvol.raiz=ambiente
     control=0
     control2=0
     for i in range(12):
         if pudePonerLinea(i, ambiente):
-            arbol.hijos.append(arbol())
-            arbol.hijos[control].raiz=ponerLinea(i, ambiente)
+            arvol.hijos.append(arbol())
+            arvol.hijos[control].raiz=ponerLinea(i, ambiente)
             for j in range(12):
-                arbol.hijos[control].hijos.append(arbol())
-                arbol.hijos[control].hijos[control2]
-                control2 = control2 + 1
-                if j == 11:
-                    control2 = 0
+                if pudePonerLinea(j, arvol.hijos[control].raiz):
+                    arvol.hijos[control].hijos.append(arbol())
+                    arvol.hijos[control].hijos[control2]=ponerLinea(i, ambiente) # falta
+                    control2 = control2 + 1
+                    if j == 11:
+                        control2 = 0
             control = control + 1
-    pass
+    return arvol
+
+
+
+'''
+ambiente2=list(ambiente)
+ambiente2[2]=5
+
+arvol=arbol()
+arvol.raiz=list(ambiente)
+arvol.hijos.append(arbol())
+arvol.hijos[0].raiz=ambiente2
+arvol.hijos[0].hijos.append(arbol())
+arvol.hijos[0].hijos[0].raiz=ambiente
+
+arbol2 = copy.deepcopy(arvol)
+arvol.hijos[0].hijos[0].raiz=ambiente2
+print(arbol2.hijos[0].hijos[0].raiz)
+print(arvol.hijos[0].hijos[0].raiz)
+'''
